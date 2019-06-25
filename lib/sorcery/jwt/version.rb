98e59f4831f24a68e9674ed8ee52c2d6889ecb1b
Keith Mattix II <keithmattix2@gmail.com>
2019-06-25 16:07:21 -0500

    Bump sorcery version and add compatibility for sorcery 0.13.0

--- lib/sorcery/jwt/version.rb
+++ lib/sorcery/jwt/version.rb
@@ -1,5 +1,5 @@
 module Sorcery
   module Jwt
-    VERSION = "0.1.6".freeze
+    VERSION = "0.1.7".freeze
   end
 end
--- spec/sorcery/jwt_spec.rb
+++ spec/sorcery/jwt_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Sorcery::Jwt do
   it "has a version number" do
     expect(Sorcery::Jwt::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
